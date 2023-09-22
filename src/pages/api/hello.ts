5ffe244e8d4e23f6912ba249da840274cd66d8c4
Manuel <hbiaser132@gmail.com>
2023-09-21 19:12:47 -0500

    fix: render links

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const { status, documentHexid }: Record<string, any> = req.query ?? {};
+
+  const query = new URLSearchParams({
+    status: status,
+    documentHexid: documentHexid
+  }).toString();
+  // QA
+  // fetch('https://backendonboardingqa.azurewebsites.net/loans/whatsignHooks', )
+  // DEV
+  // fetch('https://backendonboarding.azurewebsites.net/loans/whatsignHooks')
+  // LOCAL
+  const url = `https://khwsmpj9-8000.use2.devtunnels.ms/loans/whatsignHooks?${query}`
+  const resp = await fetch(url)
+    .catch((err) => console.log(err));
+
+  // res.send({ name: 'John Doe' })
+  res.status(200).send({ name: 'John Doe' })
 }
+
